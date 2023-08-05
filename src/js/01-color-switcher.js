b37cf8df91a48989a7e65e486650004833743e80
Iryna Burlaka <i.am.blinova@gmail.com>
2023-08-05 17:02:23 +0200

    task hw-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+
+refs.start.addEventListener('click', onClickChangeColor);
+refs.stop.addEventListener('click', offClickChangeColor);
+
+function onClickChangeColor() {
+  console.log('Change documents color');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+}
+
+function offClickChangeColor() {
+  clearInterval(timerId);
+  refs.stop.disabled = true;
+  refs.start.disabled = false;
+}
