d9b123962bb159bdedefe1390e5f6f50ab7fa798
Iryna Burlaka <i.am.blinova@gmail.com>
2023-08-08 13:33:22 +0200

    fix timeout

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -19,28 +19,28 @@ function startPromises(e) {
   step = Number(refs.form.elements.step.value);
 
   for (let i = 1; i <= amount; i += 1) {
-    setTimeout(() => {
-      createPromise(i, delay)
-        .then(({ position, delay }) => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-      delay += step;
-    }, delay);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
 
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
   return promise;
 }
